79da7bc79c2f1d0b39bdf44593c9a5d5d34aee6c
Shaurya Singh <141617574+ShauryaTEBSS@users.noreply.github.com>
2026-02-14 10:48:05 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ const app = document.querySelector<HTMLDivElement>('#app');
 
 if (app) {
   app.innerHTML = `
-    <h1>Hello TypeScript!</h1>
+    <h1>Hello TypeScripT!</h1>
     <p>The current time is: <strong>${new Date().toLocaleTimeString()}</strong></p>
     <p>Status: <span style="color: green;">Deployment Successful</span></p>
   `;
